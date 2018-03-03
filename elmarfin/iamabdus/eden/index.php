87d5641a0b51600e34dd110a26c70597dcab2296
Arcy <arcylen103095@gmail.com>
2018-03-03 22:48:18 +0800

    test

--- elmarfin/iamabdus/eden/index.php
+++ elmarfin/iamabdus/eden/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
         
   <?php     
-    $con=mysqli_connect("localhost", "root","water123");
+    $con = mysqli_connect("localhost", "root","water123");
     mysqli_select_db($con, "aplayadb"); ?>
 <head>
 
