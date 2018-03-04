c9723abf4c96c64288913fcce30a4b821190267f
Edward Guevarra <edwardguevarra2003@gmail.com>
2018-03-04 16:24:08 +0800

    Arc

--- elmarfin/iamabdus/eden/gallery-4col.php
+++ elmarfin/iamabdus/eden/gallery-4col.php
@@ -91,7 +91,7 @@
 
         
   <?php     
-    $con=mysqli_connect("localhost", "root","");
+    $con=mysqli_connect("localhost", "root","water123");
     mysqli_select_db($con, "aplayadb"); ?>
 
       <?php $catcher=mysqli_query($con, "SELECT *, typeName FROM room LEFT JOIN roomtype ON room.typeID = roomtype.typeID WHERE room.gallery_img = 1"); ?>
--- view_other_services.php
+++ view_other_services.php
@@ -104,7 +104,7 @@
 
         
   <?php     
-    $con = mysqli_connect("localhost", "root","");
+    $con = mysqli_connect("localhost", "root","water123");
     mysqli_select_db($con, "aplayadb"); ?>
 
       <?php $catcher=mysqli_query($con, "SELECT *, typeName FROM room LEFT JOIN roomtype ON room.typeID = roomtype.typeID WHERE roomtype.typeName = 'Other Services'");
